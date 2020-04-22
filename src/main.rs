15ba63402eedcb2e87782d77e881c673e1d47f8a
Jack Platten <jack.k.platten@gmail.com>
2020-04-22 15:33:32 -0700

    Receive input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter an IP address/mask in the format 192.168.1.1/24: ");
+
+    let mut input = String::new();
+    
+    io::stdin().read_line(&mut input).expect("failed to read line");
+
 }
